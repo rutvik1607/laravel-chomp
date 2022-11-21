767f23b1c11be12e76c559d6e2eb9dc1b104582d
hareshParmar <hareshdtl@gmail.com>
2022-11-21 11:09:04 +0530

    changes

--- resources/views/components/blog.blade.php
+++ resources/views/components/blog.blade.php
@@ -1,3 +1,9 @@
+<?php
+
+// use App\Http\Controllers\BlogsController;
+use App\Models\blog as Blogs;
+?>
+
 <style>
     .w-tab-menu {
         position: relative;
@@ -75,82 +81,133 @@
 <div style="opacity: 1;" class="content-section">
     <div class="container">
         <div data-duration-in="300" data-duration-out="100" data-current="Full Menu" data-easing="ease" class="w-tabs">
-            <div class="tab-menu-round w-tab-menu" role="tablist"><a data-w-tab="all"
-                    class="tab-link-round w-inline-block w-tab-link w--current" id="w-tabs-0-data-w-tab-0"
-                    href="#w-tabs-0-data-w-pane-0" role="tab" aria-controls="w-tabs-0-data-w-pane-0"
-                    aria-selected="true">
-                    <div>Full Menu</div>
-                </a><a data-w-tab="1" class="tab-link-round w-inline-block w-tab-link" tabindex="-1"
-                    id="w-tabs-0-data-w-tab-1" href="#w-tabs-0-data-w-pane-1" role="tab"
-                    aria-controls="w-tabs-0-data-w-pane-1" aria-selected="false">
-                    <div>Burgers</div>
-                </a><a data-w-tab="2" class="tab-link-round w-inline-block w-tab-link" tabindex="-1"
-                    id="w-tabs-0-data-w-tab-2" href="#w-tabs-0-data-w-pane-2" role="tab"
-                    aria-controls="w-tabs-0-data-w-pane-2" aria-selected="false">
-                    <div>Sides</div>
+            <div class="tab-menu-round w-tab-menu" role="tablist">
+                <a data-w-tab="1" class="tab-link-round w-inline-block w-tab-link w--current" id="w-tabs-0-data-w-tab-1" href="#w-tabs-0-data-w-pane-1" role="tab" aria-controls="w-tabs-0-data-w-pane-1" aria-selected="true">
+                    <div>1</div>
                 </a>
-                <a data-w-tab="3" class="tab-link-round w-inline-block w-tab-link" tabindex="-1"
-                    id="w-tabs-0-data-w-tab-3" href="#w-tabs-0-data-w-pane-3" role="tab"
-                    aria-controls="w-tabs-0-data-w-pane-3" aria-selected="false">
-                    <div>Normal</div>
+                <a data-w-tab="2" class="tab-link-round w-inline-block w-tab-link" tabindex="-1" id="w-tabs-0-data-w-tab-2" href="#w-tabs-0-data-w-pane-2" role="tab" aria-controls="w-tabs-0-data-w-pane-2" aria-selected="false">
+                    <div>2</div>
                 </a>
-                <a data-w-tab="4" class="tab-link-round w-inline-block w-tab-link" tabindex="-1"
-                    id="w-tabs-0-data-w-tab-4" href="#w-tabs-0-data-w-pane-4" role="tab"
-                    aria-controls="w-tabs-0-data-w-pane-3" aria-selected="false">
-                    <div>Scop</div>
+                <a data-w-tab="3" class="tab-link-round w-inline-block w-tab-link" tabindex="-1" id="w-tabs-0-data-w-tab-3" href="#w-tabs-0-data-w-pane-3" role="tab" aria-controls="w-tabs-0-data-w-pane-3" aria-selected="false">
+                    <div>3</div>
                 </a>
-                <a data-w-tab="5" class="tab-link-round w-inline-block w-tab-link" tabindex="-1"
-                    id="w-tabs-0-data-w-tab-5" href="#w-tabs-0-data-w-pane-5" role="tab"
-                    aria-controls="w-tabs-0-data-w-pane-3" aria-selected="false">
-                    <div>Chines</div>
+                <a data-w-tab="4" class="tab-link-round w-inline-block w-tab-link" tabindex="-1" id="w-tabs-0-data-w-tab-4" href="#w-tabs-0-data-w-pane-4" role="tab" aria-controls="w-tabs-0-data-w-pane-4" aria-selected="false">
+                    <div>4</div>
+                </a>
+                <a data-w-tab="5" class="tab-link-round w-inline-block w-tab-link" tabindex="-1" id="w-tabs-0-data-w-tab-5" href="#w-tabs-0-data-w-pane-5" role="tab" aria-controls="w-tabs-0-data-w-pane-5" aria-selected="false">
+                    <div>5</div>
                 </a>
             </div>
             <div class="w-tab-content">
-                <div data-w-tab="all" class="tab-pane-wrap w-tab-pane w--tab-active" id="w-tabs-0-data-w-pane-0"
-                    role="tabpanel" aria-labelledby="w-tabs-0-data-w-tab-0">
+                <div data-w-tab="1" class="tab-pane-wrap w-tab-pane w-tab-pane1" id="w-tabs-0-data-w-pane-1" role="tabpanel" aria-labelledby="w-tabs-0-data-w-tab-1">
                     <div class="row">
+                        @php
+                        $blogs = Blogs::where("category", '=', 1)->get();
+                        // print_r($blogs[0]['name']);exit;
+                        @endphp
+                        @foreach ($blogs as $blog => $key)
                         <div class="col-md-4 col-sm-12 col-lg-4">
                             <div class="blogs" style="border: 1px solid #eee;padding: 2rem;border-radius: 10px;">
                                 <div class="card">
-                                    <img src="https://assets.website-files.com/5e865e09d8efa3d64d76b59d/5e865e09d8efa3676276b5cd_Burger%2003.png"
-                                        class="card-img-top" alt="test">
+                                    <img src="{{url('/')}}/public/images/blog/{{ $key['image'] }}" class="card-img-top" alt="test">
                                     <div class="card-body">
-                                        <h5 class="card-title">Card title</h5>
-                                        <p class="card-text">
-                                            Some quick example text to build on the card title and make
-                                            up the bulk of the card's content.
-                                        </p>
-                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                        <h5 class="card-title">{{ $key['name'] }}</h5>
+                                        <p class="card-text">{{ $key['description'] }}</p>
+                                        <a href="{{ $key['ref1'] }}" class="btn btn-primary">Go somewhere</a>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-4 col-sm-12 col-lg-4">
-                            <div class="blogs" style="border: 1px solid #eee;padding: 2rem;border-radius: 10px;">
-
-                            </div>
-                        </div>
+                        @endforeach
                     </div>
                 </div>
-                <div data-w-tab="1" class="tab-pane-wrap w-tab-pane" id="w-tabs-0-data-w-pane-1" role="tabpanel"
-                    aria-labelledby="w-tabs-0-data-w-tab-1">
-
+                <div data-w-tab="2" class="w-tab-pane w-tab-pane2 d-none" id="w-tabs-0-data-w-pane-2" role="tabpanel" aria-labelledby="w-tabs-0-data-w-tab-2">
+                    <div class="row">
+                        @php
+                        $blogs = Blogs::where("category", '=', 2)->get();
+                        // print_r($blogs[0]['name']);exit;
+                        @endphp
+                        @foreach ($blogs as $blog => $key)
+                        <div class="col-md-4 col-sm-12 col-lg-4">
+                            <div class="blogs" style="border: 1px solid #eee;padding: 2rem;border-radius: 10px;">
+                                <div class="card">
+                                    <img src="{{url('/')}}/public/images/blog/{{ $key['image'] }}" class="card-img-top" alt="test">
+                                    <div class="card-body">
+                                        <h5 class="card-title">{{ $key['name'] }}</h5>
+                                        <p class="card-text">{{ $key['description'] }}</p>
+                                        <a href="{{ $key['ref1'] }}" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
+                    </div>
                 </div>
-                <div data-w-tab="2" class="w-tab-pane" id="w-tabs-0-data-w-pane-2" role="tabpanel"
-                    aria-labelledby="w-tabs-0-data-w-tab-2">
-
+                <div data-w-tab="3" class="w-tab-pane w-tab-pane3 d-none" id="w-tabs-0-data-w-pane-3" role="tabpanel" aria-labelledby="w-tabs-0-data-w-tab-3">
+                    <div class="row">
+                        @php
+                        $blogs = Blogs::where("category", '=', 3)->get();
+                        // print_r($blogs[0]['name']);exit;
+                        @endphp
+                        @foreach ($blogs as $blog => $key)
+                        <div class="col-md-4 col-sm-12 col-lg-4">
+                            <div class="blogs" style="border: 1px solid #eee;padding: 2rem;border-radius: 10px;">
+                                <div class="card">
+                                    <img src="{{url('/')}}/public/images/blog/{{ $key['image'] }}" class="card-img-top" alt="test">
+                                    <div class="card-body">
+                                        <h5 class="card-title">{{ $key['name'] }}</h5>
+                                        <p class="card-text">{{ $key['description'] }}</p>
+                                        <a href="{{ $key['ref1'] }}" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
+                    </div>
                 </div>
-                <div data-w-tab="3" class="w-tab-pane" id="w-tabs-0-data-w-pane-3" role="tabpanel"
-                    aria-labelledby="w-tabs-0-data-w-tab-3">
-
+                <div data-w-tab="4" class="w-tab-pane w-tab-pane4 d-none" id="w-tabs-0-data-w-pane-4" role="tabpanel" aria-labelledby="w-tabs-0-data-w-tab-4">
+                    <div class="row">
+                        @php
+                        $blogs = Blogs::where("category", '=', 4)->get();
+                        // print_r($blogs[0]['name']);exit;
+                        @endphp
+                        @foreach ($blogs as $blog => $key)
+                        <div class="col-md-4 col-sm-12 col-lg-4">
+                            <div class="blogs" style="border: 1px solid #eee;padding: 2rem;border-radius: 10px;">
+                                <div class="card">
+                                    <img src="{{url('/')}}/public/images/blog/{{ $key['image'] }}" class="card-img-top" alt="test">
+                                    <div class="card-body">
+                                        <h5 class="card-title">{{ $key['name'] }}</h5>
+                                        <p class="card-text">{{ $key['description'] }}</p>
+                                        <a href="{{ $key['ref1'] }}" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
+                    </div>
                 </div>
-                <div data-w-tab="4" class="w-tab-pane" id="w-tabs-0-data-w-pane-4" role="tabpanel"
-                    aria-labelledby="w-tabs-0-data-w-tab-4">
-
-                </div>
-                <div data-w-tab="4" class="w-tab-pane" id="w-tabs-0-data-w-pane-5" role="tabpanel"
-                    aria-labelledby="w-tabs-0-data-w-tab-5">
-
+                <div data-w-tab="5" class="w-tab-pane w-tab-pane5 d-none" id="w-tabs-0-data-w-pane-5" role="tabpanel" aria-labelledby="w-tabs-0-data-w-tab-5">
+                    <div class="row">
+                        @php
+                        $blogs = Blogs::where("category", '=', 5)->get();
+                        // print_r($blogs[0]['name']);exit;
+                        @endphp
+                        @foreach ($blogs as $blog => $key)
+                        <div class="col-md-4 col-sm-12 col-lg-4">
+                            <div class="blogs" style="border: 1px solid #eee;padding: 2rem;border-radius: 10px;">
+                                <div class="card">
+                                    <img src="{{url('/')}}/public/images/blog/{{ $key['image'] }}" class="card-img-top" alt="test">
+                                    <div class="card-body">
+                                        <h5 class="card-title">{{ $key['name'] }}</h5>
+                                        <p class="card-text">{{ $key['description'] }}</p>
+                                        <a href="{{ $key['ref1'] }}" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/front/blog.blade.php
+++ resources/views/front/blog.blade.php
@@ -1,10 +1,44 @@
 <x-front-layout>
 
-    <x-page-banner slug="{{ $slug }}" title="{{ $slug }}"
-        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
-        bannerBg="public/images/2022/08/banner-bg-2.png" bannerBtnBg="public/images/2022/08/cart-btn.svg"
-        bannerImg="public/images/2022/08/banner-1.png">
+    <x-page-banner slug="{{ $slug }}" title="{{ $slug }}" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged." bannerBg="public/images/2022/08/banner-bg-2.png" bannerBtnBg="public/images/2022/08/cart-btn.svg" bannerImg="public/images/2022/08/banner-1.png">
     </x-page-banner>
 
     <x-blog name="" title="" description="" category="" image="" ref1=""></x-blog>
 </x-front-layout>
+<script>
+    jQuery(document).ready(function($) {
+        $('.tab-menu-round').children('a').click(function() {
+            $('.w-tab-link').removeClass('w--current');
+            $(this).addClass('w--current');
+        });
+
+        $('#w-tabs-0-data-w-tab-1').click(function() {
+            $('.w-tab-pane').addClass('d-none');
+            $('.w-tab-pane1').removeClass('d-none');
+
+        });
+
+        $('#w-tabs-0-data-w-tab-2').click(function() {
+            $('.w-tab-pane').addClass('d-none');
+            $('.w-tab-pane2').removeClass('d-none');
+
+        });
+
+        $('#w-tabs-0-data-w-tab-3').click(function() {
+            $('.w-tab-pane').addClass('d-none');
+            $('.w-tab-pane3').removeClass('d-none');
+
+        });
+
+        $('#w-tabs-0-data-w-tab-4').click(function() {
+            $('.w-tab-pane').addClass('d-none');
+            $('.w-tab-pane4').removeClass('d-none');
+
+        });
+        $('#w-tabs-0-data-w-tab-5').click(function() {
+            $('.w-tab-pane').addClass('d-none');
+            $('.w-tab-pane5').removeClass('d-none');
+
+        });
+    });
+</script>
